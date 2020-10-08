9e89d368ca46d495dedf573359c77c560cac2cd5
Patato777 <66975138+Patato777@users.noreply.github.com>
2020-10-08 13:56:07 +0200

    Plot inside image + long/lat instead of y/x

--- display_results.py
+++ display_results.py
@@ -6,12 +6,13 @@ class Map:
         self.canvas = canvas
         self.canvas.pack()
         corners,img = self.choosemap(minc,maxc)
+        print(corners,corners[0][0])
         self.img = PhotoImage(file=f'.{os.sep}resources{os.sep}{img}')
         self.imgh,self.imgw = self.img.height(),self.img.width()
         self.canvas.config(height=self.imgh, width=self.imgw)
         self.id = self.canvas.create_image(self.imgw//2,self.imgh//2,image=self.img)
-        self.scale = self.img.width()/(corners[1][0]-corners[0][0])
-        self.minx,self.miny = corners[0][0],corners[0][1]
+        self.scale = self.img.width()/(corners[0][0]-corners[1][0])
+        self.minlong,self.minlat = corners[1][0],corners[0][1]
 
     def choosemap(self,minc,maxc) :
         print(minc,maxc)
@@ -19,13 +20,14 @@ class Map:
             mapindex = eval(f.read())
         included = lambda corners : minc[0]>corners[1][0] and minc[1]>corners[0][1] and maxc[0]<corners[0][0] and maxc[1]<corners[1][1]
         possible = [key for key in mapindex.keys() if included(key)]
-        bmap = max(possible,key = lambda item : (item[1][0]-item[0][0])*(item[1][1]-item[0][1]))
-        return bmap,mapindex[bmap]
+        bestmap = min(possible,key = lambda item : (item[1][0]-item[0][0])*(item[1][1]-item[0][1]))
+        return bestmap,mapindex[bestmap]
 
-    def plot (self,x,y,label) :
-        px,py = self.scale*x - self.minx, self.scale*y - self.miny
-        ov = self.canvas.create_oval(px-1,py-1,px+1,py+1)
-        lbl = self.canvas.create_text(px,py+5,text=label)
+    def plot (self,long,lat,label) :
+        plong,plat = self.imgh-self.scale*(long - self.minlong), self.scale*(lat - self.minlat)
+        #TODO: Correct plat
+        ov = self.canvas.create_oval(plat-1,plong-1,plat+1,plong+1)
+        lbl = self.canvas.create_text(plat,plong+5,text=label)
         return ov,lbl
 
     def colorise (self, node, color):
