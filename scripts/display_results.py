199511ede1011f7c0d27a498faefbe4657a25ad4
Patato777 <jean.sma@free.fr>
2020-10-22 18:06:12 +0200

    Commenting

--- scripts/display_results.py
+++ scripts/display_results.py
@@ -1,42 +1,47 @@
 from tkinter import *
 import os,logging
 
-class Map:
+class Map:#Graphic interface
     def __init__(self,canvas,minc,maxc):
         self.canvas = canvas
         self.canvas.pack()
-        corners,img = self.choosemap(minc,maxc)
+        corners,img = self.choosemap(minc,maxc)#Coord of the corners of the map and the corresponding picture
         logging.debug(str((corners,corners[0][0])))
         self.img = PhotoImage(file=f'.{os.sep}resources{os.sep}{img}')
-        self.imgh,self.imgw = self.img.height(),self.img.width()
+        self.imgh,self.imgw = self.img.height(),self.img.width()#Size of the picture
+        #Displaying the map
         self.canvas.config(height=self.imgh, width=self.imgw)
         self.id = self.canvas.create_image(self.imgw//2,self.imgh//2,image=self.img)
-        self.scale = self.img.width()/(corners[0][0]-corners[1][0])
+        self.scale = self.img.width()/(corners[0][0]-corners[1][0])#Scale of the map
         self.minlong,self.minlat = corners[1][0],corners[0][1]
 
-    def choosemap(self,minc,maxc) :
+    def choosemap(self,minc,maxc) :#To choose the best map, given the places
         logging.debug(str((minc,maxc)))
         with open(f'.{os.sep}resources{os.sep}dimmap.txt') as f :
             mapindex = eval(f.read())
         included = lambda corners : minc[0]>corners[1][0] and minc[1]>corners[0][1] and maxc[0]<corners[0][0] and maxc[1]<corners[1][1]
+        #Best map among those which contain all the points
         possible = [key for key in mapindex.keys() if included(key)]
         bestmap = min(possible,key = lambda item : (item[1][0]-item[0][0])*(item[1][1]-item[0][1]))
         return bestmap,mapindex[bestmap]
 
-    def plot (self,long,lat,label) :
+    def plot (self,long,lat,label) :#To plot a point
+        #Calculating the coords on the picture
         plong,plat = self.imgh-self.scale*(long - self.minlong), self.scale*(lat - self.minlat)
         #TODO: Correct plat
+        #Displaying the place among with its nam
         ov = self.canvas.create_oval(plat-1,plong-1,plat+1,plong+1)
         lbl = self.canvas.create_text(plat,plong+5,text=label)
         return ov,lbl
 
-    def colorise (self, node, color):
+    def colorise (self, node, color):#To change the color of a point
         for elem in node :
             self.canvas.itemconfig(elem,fill=color)
 
-class Population:
+class Population:#The class of the nodes
     def __init__(self,population) :
         self.pop = population
+        #The most extreme coords
         self.min = (min(self.pop,key = lambda item : item[0])[0],min(self.pop,key = lambda item : item[1])[1])
         self.max = (max(self.pop,key = lambda item : item[0])[0],max(self.pop,key = lambda item : item[1])[1])
         logging.debug(str((self.pop,self.min)))
