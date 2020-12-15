0ba139a250c7f5e8b8ccf59b6f1f06f2d5b34f5e
Patato777 <jean.sma@free.fr>
2020-12-15 20:28:19 +0100

    Completed the CAH method

--- scripts/baseseeds.py
+++ scripts/baseseeds.py
@@ -1,5 +1,5 @@
 import random
-from numpy import array
+from numpy import array,delete,copy
 
 def verif(seeds) :
     for seed in seeds :
@@ -28,10 +28,23 @@ def kmpp(self,k) :
 
 def cah(self,k):
     dist_mat = array([[v.length for v in n.vertices.values()] for n in self.nodes])
+    all_dist = copy(dist_mat)
+    clusters = {k:[k] for k in range(len(dist_mat))}
     for i in range(len(dist_mat)-k) :
         n = len(dist_mat)
-        group = min([[(x,y) for x in range(n) if x!=y] for y in range(n)], key=lambda i:dist_mat[i[0],i[1]])
+        group = min([(x,y) for x in range(n) for y in range(n) if x!=y], key=lambda i:dist_mat[i[0],i[1]])
         if group[0] == 0 :
-            pass#Swap columns (a[:, [c0, c1]] = a[:, [c1, c0]]) and rows (a[[c0,c1]]=a[[c1,c0]])
-        #TODO: remove a row/column and recalculate distances
-       
+            near = 1
+        else :
+            near = -1
+        dist_mat[:,[group[0]+near,group[1]]] = dist_mat[:,[group[1],group[0]+near]]
+        dist_mat[[group[0]+near,group[1]]]=dist_mat[[group[1],group[0]+near]]
+        clusters[group[0]+near],clusters[group[1]] = clusters[group[1]],clusters[group[0]+near]
+        new_dist = [(dist_mat[group[0],k]+dist_mat[group[0]+near,k])/2 for k in range(len(dist_mat))]
+        dist_mat[group[0]],dist_mat[:,group[0]] = new_dist,new_dist
+        dist_mat=delete(delete(dist_mat,group[0]+near,0),group[0]+near,1)
+        clusters[group[0]].extend(clusters[group[0]+near])
+        for j in range(group[0]+near,n-1) :
+            clusters[j],clusters[j+1] = clusters[j+1],clusters[j]
+        clusters.pop(n-1)
+    return [min([k for k in values],key=lambda k:sum([all_dist[k,n] for n in values])) for values in clusters.values()]
