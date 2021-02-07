2ae796c98f208ad83a31fe1f2bf7b23e7e92b492
Patato777 <jean.sma@free.fr>
2021-02-07 12:08:53 +0100

    Added a script to find the exact solution, with a test

--- scripts/exact.py
+++ scripts/exact.py
@@ -1,43 +1,38 @@
 import os
 import numpy as np
 
-with open(os.path.abspath('./tests/resources/Table_distances_Essonne_py.txt'), 'r') as f:
-    dist_mat = eval(f.read())
 
-
-def total(pool):
+def total(pool, dist_mat):
     return sum([dist_mat[i][j] for c, i in enumerate(pool) for j in pool[c + 1:]])
 
 
-def func(n, k):
+def best_sol(n, k, dist_mat):
     table = np.zeros((n // k, k), dtype=int)
-    return recfunc(0, 1, n // k, k, table, list(range(1, n)), 0, 0)
+    return constr_sol_rec(0, 1, n // k, k, table, list(range(1, n)), 0, dist_mat)
 
 
-def recfunc(col, lig, p, k, table, rest, tot, prof):
+def constr_sol_rec(col, lig, p, k, table, rest, tot, dist_mat):
     table2 = np.copy(table)
     if lig == 0 and col == len(table[0]) - 1:
         table2[:, col] = rest
-        return table2, tot + total(table[:, col])
+        return table2, tot + total(table2[:, col], dist_mat)
     elif lig == 0:
         beg, end = table[0, col - 1] + 1, col * p + 1
-        tot += total(table[:, col - 1])
+        tot += total(table[:, col - 1], dist_mat)
     else:
-        beg, end = table[lig - 1, col] + 1, k * (p - 1) + lig
+        beg, end = table[lig - 1, col] + 1, k * (p - 1) + lig + 2
     res = list()
     for i in range(beg, end):
         if i in rest:
             rest2 = rest.copy()
             rest2.remove(i)
             table2[lig, col] = i
-            rec = recfunc(col + (lig + 1) // p, (lig + 1) % p, p, k, table2, rest2, tot, prof + 1)
+            rec = recfunc(col + (lig + 1) // p, (lig + 1) % p, p, k, table2, rest2, tot, dist_mat)
             res.append(rec)
     try:
         return min(res, key=lambda r: r[1])
     except Exception as error:
-        return table,tot**2
-    # print(f'lig={lig},col={col},beg={beg},end={end},rest={rest},table={table}')
+        return table, np.inf
+        # print(f'lig={lig},col={col},beg={beg},end={end},rest={rest},table={table}')
+        # assert False
 
-
-test = func(20, 5)
-print(test)
