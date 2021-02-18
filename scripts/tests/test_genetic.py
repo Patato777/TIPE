00346322d1aed3057ea9c8636033668ef8937085
Patato777 <jean.sma@free.fr>
2021-02-18 12:11:57 +0100

    Tests for the genetic algorithm

---
+++ scripts/tests/test_genetic.py
@@ -0,0 +1,8 @@
+import scripts.genetic as gen
+import scripts.operators as op
+
+c1 = gen.Chromosome([1, 2, 3, 4, 5, 6, 7, 8], 8)
+c2 = gen.Chromosome([2, 4, 6, 8, 1, 3, 5, 7], 8)
+
+cross = op.Cross('pmx')
+print(cross.cross(c1, c2))
