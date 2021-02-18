8b398fdf1de4fcd72356754424f60ef3df0a05ff
Patato777 <jean.sma@free.fr>
2021-02-18 17:27:02 +0100

    Now with proper logging

--- scripts/tests/test_genetic.py
+++ scripts/tests/test_genetic.py
@@ -1,8 +1,9 @@
 import scripts.genetic as gen
 import scripts.operators as op
+import logging
 
 c1 = gen.Chromosome([1, 2, 3, 4, 5, 6, 7, 8], 8)
 c2 = gen.Chromosome([2, 4, 6, 8, 1, 3, 5, 7], 8)
 
 cross = op.Cross('pmx')
-print(cross.cross(c1, c2))
+logging.info(cross.cross(c1, c2))
