ea98c38fbc95be396c927948ec2a34a794244a4b
Patato777 <jean.sma@free.fr>
2021-03-25 14:58:14 +0100

    Rewriting it totally to make it cleaner

--- scripts/display_genetic.py
+++ scripts/display_genetic.py
@@ -4,23 +4,31 @@ import os
 import configparser
 import logging
 
-conf = configparser.ConfigParser()
-conf.read(dirname + '/resources/config')
-config = conf['disp_gen']
-
 dirname = os.path.dirname(__file__)
 
-conn = sqlite3.connect(dirname + '/resources/genetic.db')
-curs = conn.cursor()
+class Config:
+    def __init__(self):
+        conf = configparser.ConfigParser()
+        conf.read(dirname + '/resources/config')
+        self.config = conf['disp_gen']
 
-p_set = eval(config['p_set'])
+class Database:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(dirname + f'/resources/{db}.db')
+        self.curs = self.conn.cursor()
 
-cond = ' AND '.join([key+'='+val for key, val in p_set])
-logging.debug(cond)
+class Main:
+    def __init__(self):
+        self.config = Config().config
+        self.db = Database(self.config['DB'])
 
-records = curs.execute(f'SELECT {config["generation"]},{config["score"]} FROM {config["TABLE"]} WHERE ' + cond)
-
-for rec in records:
-    pl.plot(int(rec[0]),int(rec[1]))
-
-pl.show()
+    def display(self):
+        self.p_set = eval(self.config['p_set'])
+        logging.debug(self.p_set.items())
+        cond = ' AND '.join([key+'="'+str(val)+'"' for key, val in self.p_set.items()])
+        logging.debug(cond)
+        self.records = self.db.curs.execute(f'SELECT generation,scores FROM {self.config["TABLE"]} WHERE ' + cond)
+        for rec in self.records:
+            logging.debug(rec)
+            pl.plot(int(rec[0]),int(rec[1]))
+        pl.show()
