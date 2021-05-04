3423db381723a61b52c0342132a6300b9916653d
Patato777 <jean.sma@free.fr>
2021-05-04 14:26:26 +0200

    Tests are now in a function (easier to call)

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,9 @@
 import logging
 import time
 
+import scripts.tests.test_genetic as test
+
 logging.basicConfig(filename='scripts/tests/resources/test.log', level=logging.INFO)
 logging.info(f'--------- {time.asctime()} ----------')
 
-import scripts.tests.test_genetic
+test.test()
--- scripts/tests/test_2opt.py
+++ scripts/tests/test_2opt.py
@@ -4,19 +4,20 @@ import os
 
 dirname = os.path.dirname(__file__)
 
-with open(dirname+'/resources/Table_distances_Essonne_py.txt','r') as f :
-    dist_mat = eval(f.read())
+def test():
+    with open(dirname+'/resources/Table_distances_Essonne_py.txt','r') as f :
+        dist_mat = eval(f.read())
 
-with open(dirname + '/resources/Liste_pos_Essonne_py.txt', 'r') as f:
-    pop = eval(f.read())
+    with open(dirname + '/resources/Liste_pos_Essonne_py.txt', 'r') as f:
+        pop = eval(f.read())
 
-with open(dirname + '/resources/liste_essonne_py.txt') as f:
-    names = eval(f.read())
+    with open(dirname + '/resources/liste_essonne_py.txt') as f:
+        names = eval(f.read())
 
-two_opt = Two_opt(dist_mat, 7)
-pools = two_opt.two_opt()
+    two_opt = Two_opt(dist_mat, 7)
+    pools = two_opt.two_opt()
 
-main_display = dr.Main(pop, names)
-main_display.dispool(pools)
+    main_display = dr.Main(pop, names)
+    main_display.dispool(pools)
 
-main_display.root.mainloop()
+    main_display.root.mainloop()
