087049cea3d7ec54646c41753eb8c3a423adea58
Patato777 <jean.sma@free.fr>
2021-05-05 18:47:23 +0200

    Now fetching all the matching records at once, then processing them

--- scripts/display_genetic.py
+++ scripts/display_genetic.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sqlite3
+import statistics
 
 import pylab as pl
 
@@ -23,23 +24,31 @@ class Main:
         cond = ' AND '.join([key + '="' + str(val) + '"' for key, val in self.p_set.items()])
         selection = f'SELECT generation,scores FROM {table} WHERE '
         logging.debug(cond)
-        gen = 0
-        lower, mid, upper = list(), list(), list()
-        while self.db.curs.execute(selection + cond + f' AND generation="{gen}" LIMIT 1').fetchone() is not None:
-            records = self.db.curs.execute(selection + cond + f' AND generation="{gen}"')
-            avg = 0
-            mini, maxi = float('inf'), 0
-            for c, rec in enumerate(records):
-                avg += int(rec[1])
-                mini = min(int(rec[1]), mini)
-                maxi = max(int(rec[1]), maxi)
-            avg /= c
-            lower.append(mini)
-            mid.append(avg)
-            upper.append(maxi)
-            gen += 1
-        gen_axis = range(gen - 1)
-        pl.plot(gen_axis, lower)
-        pl.plot(gen_axis, mid)
-        pl.plot(gen_axis, upper)
-        pl.show()
+        lower, mean, upper, median = list(), list(), list(), list()
+        records = self.db.curs.execute(selection + cond).fetchall()
+        logging.debug('Fetched')
+        try:
+            srecords = sorted(records, key=lambda r: int(r[0]))
+            if not records[0][1].startswith('['):
+                records = list(srecords)
+                records = [(k, [int(rec[1]) for rec in records[1000 * k:1000 * (k + 1)]]) for k in range(len(records) // 1000)]
+            else:
+                records = [(int(rec[0]), eval(rec[1])) for rec in srecords]
+            logging.debug('Sorted')
+            for record in records:
+                avg, med = statistics.mean(record[1]), statistics.median(record[1])
+                mini, maxi = min(record[1]), max(record[1])
+                lower.append(mini)
+                mean.append(avg)
+                median.append(med)
+                upper.append(maxi)
+            gen_axis = range(len(records))
+            pl.plot(gen_axis, lower)
+            pl.plot(gen_axis, mean)
+            pl.plot(gen_axis, median)
+            pl.plot(gen_axis, upper)
+            pl.show()
+        except Exception as error:
+            with open(dirname + '/tests/resources/rec.txt', 'w') as f:
+                f.write(str(records))
+            logging.error(error)
